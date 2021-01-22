ef5969edf2998fd30b14cea29e2eb835fa58c889
DuncanBH <2031593@champlaincollege.qc.ca>
2021-01-22 15:06:07 -0500

    Lab01 for real this time

---
+++ Lab 1/2ndFunctionsAndAlert/script.js
@@ -0,0 +1,20 @@
+function sum(a,b){
+    let c = 0;
+    c = a + b;
+    alert("The sum is: " + c);
+}
+function sub(a,b){
+    let c = 0;
+    c = a - b;
+    alert("The difference is: " + c);
+}
+function mult(a,b){
+    let c = 0;
+    c = a * b;
+    alert("The product is: " + c);
+}
+function divi(a,b){
+    let c = 0;
+    c = a / b;
+    alert("The quotient is: " + c);
+}
