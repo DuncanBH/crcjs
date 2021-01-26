a8cfc2644cc9167e4f272d2a8d4228e98bc8ac3c
DuncanBH <2031593@champlaincollege.qc.ca>
2021-01-26 14:57:45 -0500

    Lab 2 Complete

---
+++ Lab 2/2ndFormAddress/scripts.js
@@ -0,0 +1,13 @@
+function setHomeAdr(){
+    document.getElementById("streetinput").value = "1 Main St.";
+    document.getElementById("cityinput").value = "Sicilia";
+    document.getElementById("stateinput").value = "MA";
+    document.getElementById("zipinput").value = "02103";
+}
+
+function setWorkAdr() {
+    document.getElementById("streetinput").value = "15 Columbine Ln.";
+    document.getElementById("cityinput").value = "Crab City";
+    document.getElementById("stateinput").value = "MA";
+    document.getElementById("zipinput").value = "02104";
+}
