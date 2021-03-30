24096cb0f26a4f10fcfcf4ce63bd258cc7dd6ab3
DuncanBH <2031593@champlaincollege.qc.ca>
2021-03-30 14:10:58 -0400

    Lab 10

--- ss10Server2_Node/server3.js
+++ ss10Server2_Node/server3.js
@@ -22,6 +22,29 @@ app.get('/hello', function (req, res) {
     return;
 });
 
+app.get('/getBusInfo', function (req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    let dataToSend = `<div class="event">
+            <img src="img/map-ca.png" alt="Map of California" />
+            <p><b>San Francisco, CA</b><br>
+            May 1</p>
+        </div>
+        <div class="event">
+            <img src="img/map-tx.png" alt="Map of Texas" />
+            <p><b>Austin, TX</b><br>
+            May 15</p>
+        </div>
+        <div class="event">
+            <img src="img/map-ny.png" alt="Map of New York" />
+            <p><b>New York, NY</b><br>
+            May 30</p>
+        </div>
+        `
+  
+        res.send( dataToSend );
+    return;
+});
+
 var server = app.listen(PORT, function () {
    var host = server.address().address
    var port = server.address().port
